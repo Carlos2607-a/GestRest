ea702528811b3ab5e9db4ac5b6766e2ce6ceb4e6
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-04-03 20:55:32 +0000

    as

--- productos.py
+++ productos.py
@@ -15,6 +15,35 @@ restaurantes = {
         'Tinto' : ['Viña Vieja', 'Entrechuelos', 'Love By Leoube', 'Habla del Silencio', 'Vallehondo', 'Puech-Haut', 'Mas Picosa Bio', 'Hacienda Monasterio', 'Protos 27', 'Un Joben Ribera', 'Hacienda Monasterio Magnum', 'Flor de Pingus', 'Vega Sicilia Valbuena 5º año', 'Pago de Quintana', 'Roda I Reserva', 'La Mateo Vendimia', 'La Mateo Vendimia Magnum', 'Finca Besaya Joven', 'Peña El Gato', 'Pago El Espino', 'Tadeo', 'Valeriano', 'Almirez','Bon Jan Negre', 'Pasion de Bobal' ],
         'Espumoso' : ['Pet Nat Natural', 'Pasion Gran Reserva', 'Laurent Perrier Blanc', 'Laurent Perrier Rose', 'Laurent Perrier Blanc de bkancs 750 ml', 'Pasion Cava BLanco', 'Pasion Cava Rose', 'Prosecco', 'Lume' ],
         },
+        
+        'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
+        'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
+        'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
+        'Zumos': ['Manzana 20cl', 'Melocoton 20cl', 'Tomate 20cl', 'Arandanos', 'Piña 20cl','Manzana Carton 1L', 'Naranja Carton 1L', 'Melocoton Carton 1L', 'Tomate Carton 1L', 'Arandanos Carton 1L', 'Piña Carton 1L'],
+        'Infuciones' : ['Manzanilla', 'Verde', 'Rojo', 'Frutos del Bosque', 'Ingles', 'Negro', 'Albahaca', 'Menta Poleo', 'Tila'],
+        'Productos de Cafeteria': ['Leche', 'Leche de Avellanas', 'Cafe en Granos', 'Cafe en Granos Descafeinado', 'Galletas', 'Azucar Morena', 'Azucar Blanca', 'Sacarina', 'Colacao Sobres', 'Colacao Personal'],
+        'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
+        'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
+        'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    },
+    'Restaurante Andy´S': {
+        'Alcohol': {
+            'Vodka': ['Smirnoff', 'Absolut', 'Grey Goose', 'Beldever', 'Nero'],
+            'Ron': ['Barcelo','Brugal','Cacique', 'Pampero', 'Matusalen 15', 'Bacardi', ' Zakapa', 'Havana 7', 'Havana Club 3 Años', 'Malibu', 'Pampero Aniversario', 'Bacardi'],
+            'Whisky': [' Dewar´s White', 'Red Label', 'Black Label', 'Ballantines', 'Four Roses', 'Jack Daniels', 'Macallan', 'Jack Daniel', 'DYC8', 'J&B', 'Jameson Irlandes', 'Southern Comfort'],
+            'Ginebra': ['Larios', 'Beeafeter', 'Tankeray', 'Puerto de Indias', 'Bombay Saphire', 'Seagrams', 'Gordon´s', 'Hendriks', 'Nordes','Tanquerai Sevilla', 'GVine', 'Gin Mare', 'Martin Millers'],
+            'Brandy': ['Carlos I','Carlos III', '1866', 'Soberano', 'Magno', 'Remy Martin', '103', 'Courvoi'],
+            'Licores' : ['Ruavieja Hierbas', 'Crema de Orujo', 'Limonchello', 'Amaretto Disaronno', 'Cointreau', 'Tia Maria', 'Kahlua', 'Frangelico', 'Pacharan', 'Licor 43', 'Baileys', 'Jagermeister', 'Grand Manier Roja', 'Drambuie', 'Vodka Caramelo', 'Anis Seco', 'Anis Mono Dulce', 'Jose Cuervo Reposado', 'Jalisco Dorado','Jalisco Dorado Reposado','Flor de Saúco Saint Germain', 'Melocoton Sin Alcohol', 'Crema Orujo 3L', ' Licor de Hierbas 3L', 'Pacharan 3L'],
+            'Aperitivos' : ['Campari', 'Ricard', 'Aperol', 'Martini Rojo', 'Martini Bianco', 'Vermut Petroni Rojo', 'Vermut Petroni Blanco', 'Fernet Branca']
+        },
+        'Vinos' : {
+        'Blanco' : ['Pasion BIO Seco', 'Villanueva','Chan de Rosas','Chan de Rosas Magnum', 'Enate 2-3-4', 'Chablis', 'Entrechuelos', 'Mas Picosa Blanco', 'Pragustus', 'Abadia Marcier', 'Patito Feo', 'Max Ferd Richter Zepellin', 'Equilibrio', 'La Mateo Blanco Barrica', 'Menade Ecologico', 'Menade Magnum', 'Marques de Riscal Rueda Limousin', 'Puech-Haut', 'Love By Leoube', 'Libero Velo de Flor', 'Gerard Boulay', 'Finca La Olma', 'Bon Jan Blanc'],
+        'Rosado' : ['Pasion Bio', 'Love By Leoube', 'Protos Rosado', 'Love Rose Magnum', 'Clavellina', 'Puech-Haut', 'Fradei', 'Abadia Mercier', 'Lerin'],
+        'Tinto' : ['Viña Vieja', 'Entrechuelos', 'Love By Leoube', 'Habla del Silencio', 'Vallehondo', 'Puech-Haut', 'Mas Picosa Bio', 'Hacienda Monasterio', 'Protos 27', 'Un Joben Ribera', 'Hacienda Monasterio Magnum', 'Flor de Pingus', 'Vega Sicilia Valbuena 5º año', 'Pago de Quintana', 'Roda I Reserva', 'La Mateo Vendimia', 'La Mateo Vendimia Magnum', 'Finca Besaya Joven', 'Peña El Gato', 'Pago El Espino', 'Tadeo', 'Valeriano', 'Almirez','Bon Jan Negre', 'Pasion de Bobal' ],
+        'Espumoso' : ['Pet Nat Natural', 'Pasion Gran Reserva', 'Laurent Perrier Blanc', 'Laurent Perrier Rose', 'Laurent Perrier Blanc de bkancs 750 ml', 'Pasion Cava BLanco', 'Pasion Cava Rose', 'Prosecco', 'Lume' ],
+        },
+        
         'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
         'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
         'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
