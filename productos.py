692585b76b6420cae0f456410a69435a0b576078
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2025-03-12 10:41:25 +0000

    act

--- productos.py
+++ productos.py
@@ -1,4 +1,60 @@
 restaurantes = {
+    'Restaurante Symbad': {
+        'Alcohol': {
+            'Vodka': ['Smirnoff', 'Absolut', 'Grey Goose', 'Beldever', 'Nero'],
+            'Ron': ['Barcelo','Brugal','Cacique', 'Pampero', 'Matusalen 15', 'Bacardi', ' Zakapa', 'Havana 7', 'Havana Club 3 Años', 'Malibu', 'Pampero Aniversario', 'Bacardi'],
+            'Whisky': [' Dewar´s White', 'Red Label', 'Black Label', 'Ballantines', 'Four Roses', 'Jack Daniels', 'Macallan', 'Jack Daniel', 'DYC8', 'J&B', 'Jameson Irlandes', 'Southern Comfort'],
+            'Ginebra': ['Larios', 'Beeafeter', 'Tankeray', 'Puerto de Indias', 'Bombay Saphire', 'Seagrams', 'Gordon´s', 'Hendriks', 'Nordes','Tanquerai Sevilla', 'GVine', 'Gin Mare', 'Martin Millers'],
+            'Brandy': ['Carlos I','Carlos III', '1866', 'Soberano', 'Magno', 'Remy Martin', '103', 'Courvoi'],
+            'Licores' : ['Ruavieja Hierbas', 'Crema de Orujo', 'Limonchello', 'Amaretto Disaronno', 'Cointreau', 'Tia Maria', 'Kahlua', 'Frangelico', 'Pacharan', 'Licor 43', 'Baileys', 'Jagermeister', 'Grand Manier Roja', 'Drambuie', 'Vodka Caramelo', 'Anis Seco', 'Anis Mono Dulce', 'Jose Cuervo Reposado', 'Jalisco Dorado','Jalisco Dorado Reposado','Flor de Saúco Saint Germain', 'Melocoton Sin Alcohol', 'Crema Orujo 3L', ' Licor de Hierbas 3L', 'Pacharan 3L'],
+            'Aperitivos' : ['Campari', 'Ricard', 'Aperol', 'Martini Rojo', 'Martini Bianco', 'Vermut Petroni Rojo', 'Vermut Petroni Blanco', 'Fernet Branca']
+        },
+        'Vinos' : {
+        'Blanco' : ['Pasion BIO SECCC', 'Villanueva','Chan de Rosas','Chan de Rosas Magnum', 'Enate 2-3-4', 'Chablis', 'Entrechuelos', 'Mas Picosa Blanco', 'Pragustus', 'Abadia Marcier', 'Patito Feo', 'Max Ferd Richter Zepellin', 'Equilibrio', 'La Mateo Blanco Barrica', 'Menade Ecologico', 'Menade Magnum', 'Marques de Riscal Rueda Limousin', 'Puech-Haut', 'Love By Leoube', 'Libero Velo de Flor', 'Gerard Boulay', 'Finca La Olma', 'Bon Jan Blanc'],
+        'Rosado' : ['Pasion Bio', 'Love By Leoube', 'Protos Rosado', 'Love Rose Magnum', 'Clavellina', 'Puech-Haut', 'Fradei', 'Abadia Mercier', 'Lerin'],
+        'Tinto' : ['Viña Vieja', 'Entrechuelos', 'Love By Leoube', 'Habla del Silencio', 'Vallehondo', 'Puech-Haut', 'Mas Picosa Bio', 'Hacienda Monasterio', 'Protos 27', 'Un Joben Ribera', 'Hacienda Monasterio Magnum', 'Flor de Pingus', 'Vega Sicilia Valbuena 5º año', 'Pago de Quintana', 'Roda I Reserva', 'La Mateo Vendimia', 'La Mateo Vendimia Magnum', 'Finca Besaya Joven', 'Peña El Gato', 'Pago El Espino', 'Tadeo', 'Valeriano', 'Almirez','Bon Jan Negre', 'Pasion de Bobal' ],
+        'Espumoso' : ['Pet Nat Natural', 'Pasion Gran Reserva', 'Laurent Perrier Blanc', 'Laurent Perrier Rose', 'Laurent Perrier Blanc de bkancs 750 ml', 'Pasion Cava BLanco', 'Pasion Cava Rose', 'Prosecco', 'Lume' ],
+        },
+        
+        'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
+        'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
+        'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
+        'Zumos': ['Manzana 20cl', 'Melocoton 20cl', 'Tomate 20cl', 'Arandanos', 'Piña 20cl','Manzana Carton 1L', 'Naranja Carton 1L', 'Melocoton Carton 1L', 'Tomate Carton 1L', 'Arandanos Carton 1L', 'Piña Carton 1L'],
+        'Infuciones' : ['Manzanilla', 'Verde', 'Rojo', 'Frutos del Bosque', 'Ingles', 'Negro', 'Albahaca', 'Menta Poleo', 'Tila'],
+        'Productos de Cafeteria': ['Leche', 'Leche de Avellanas', 'Cafe en Granos', 'Cafe en Granos Descafeinado', 'Galletas', 'Azucar Morena', 'Azucar Blanca', 'Sacarina', 'Colacao Sobres', 'Colacao Personal'],
+        'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
+        'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
+        'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    },
+    'Restaurante Andy´S': {
+        'Alcohol': {
+            'Vodka': ['Smirnoff', 'Absolut', 'Grey Goose', 'Beldever', 'Nero'],
+            'Ron': ['Barcelo','Brugal','Cacique', 'Pampero', 'Matusalen 15', 'Bacardi', ' Zakapa', 'Havana 7', 'Havana Club 3 Años', 'Malibu', 'Pampero Aniversario', 'Bacardi'],
+            'Whisky': [' Dewar´s White', 'Red Label', 'Black Label', 'Ballantines', 'Four Roses', 'Jack Daniels', 'Macallan', 'Jack Daniel', 'DYC8', 'J&B', 'Jameson Irlandes', 'Southern Comfort'],
+            'Ginebra': ['Larios', 'Beeafeter', 'Tankeray', 'Puerto de Indias', 'Bombay Saphire', 'Seagrams', 'Gordon´s', 'Hendriks', 'Nordes','Tanquerai Sevilla', 'GVine', 'Gin Mare', 'Martin Millers'],
+            'Brandy': ['Carlos I','Carlos III', '1866', 'Soberano', 'Magno', 'Remy Martin', '103', 'Courvoi'],
+            'Licores' : ['Ruavieja Hierbas', 'Crema de Orujo', 'Limonchello', 'Amaretto Disaronno', 'Cointreau', 'Tia Maria', 'Kahlua', 'Frangelico', 'Pacharan', 'Licor 43', 'Baileys', 'Jagermeister', 'Grand Manier Roja', 'Drambuie', 'Vodka Caramelo', 'Anis Seco', 'Anis Mono Dulce', 'Jose Cuervo Reposado', 'Jalisco Dorado','Jalisco Dorado Reposado','Flor de Saúco Saint Germain', 'Melocoton Sin Alcohol', 'Crema Orujo 3L', ' Licor de Hierbas 3L', 'Pacharan 3L'],
+            'Aperitivos' : ['Campari', 'Ricard', 'Aperol', 'Martini Rojo', 'Martini Bianco', 'Vermut Petroni Rojo', 'Vermut Petroni Blanco', 'Fernet Branca']
+        },
+        'Vinos' : {
+        'Blanco' : ['Pasion BIO Seco', 'Villanueva','Chan de Rosas','Chan de Rosas Magnum', 'Enate 2-3-4', 'Chablis', 'Entrechuelos', 'Mas Picosa Blanco', 'Pragustus', 'Abadia Marcier', 'Patito Feo', 'Max Ferd Richter Zepellin', 'Equilibrio', 'La Mateo Blanco Barrica', 'Menade Ecologico', 'Menade Magnum', 'Marques de Riscal Rueda Limousin', 'Puech-Haut', 'Love By Leoube', 'Libero Velo de Flor', 'Gerard Boulay', 'Finca La Olma', 'Bon Jan Blanc'],
+        'Rosado' : ['Pasion Bio', 'Love By Leoube', 'Protos Rosado', 'Love Rose Magnum', 'Clavellina', 'Puech-Haut', 'Fradei', 'Abadia Mercier', 'Lerin'],
+        'Tinto' : ['Viña Vieja', 'Entrechuelos', 'Love By Leoube', 'Habla del Silencio', 'Vallehondo', 'Puech-Haut', 'Mas Picosa Bio', 'Hacienda Monasterio', 'Protos 27', 'Un Joben Ribera', 'Hacienda Monasterio Magnum', 'Flor de Pingus', 'Vega Sicilia Valbuena 5º año', 'Pago de Quintana', 'Roda I Reserva', 'La Mateo Vendimia', 'La Mateo Vendimia Magnum', 'Finca Besaya Joven', 'Peña El Gato', 'Pago El Espino', 'Tadeo', 'Valeriano', 'Almirez','Bon Jan Negre', 'Pasion de Bobal' ],
+        'Espumoso' : ['Pet Nat Natural', 'Pasion Gran Reserva', 'Laurent Perrier Blanc', 'Laurent Perrier Rose', 'Laurent Perrier Blanc de bkancs 750 ml', 'Pasion Cava BLanco', 'Pasion Cava Rose', 'Prosecco', 'Lume' ],
+        },
+        
+        'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
+        'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
+        'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
+        'Zumos': ['Manzana 20cl', 'Melocoton 20cl', 'Tomate 20cl', 'Arandanos', 'Piña 20cl','Manzana Carton 1L', 'Naranja Carton 1L', 'Melocoton Carton 1L', 'Tomate Carton 1L', 'Arandanos Carton 1L', 'Piña Carton 1L'],
+        'Infuciones' : ['Manzanilla', 'Verde', 'Rojo', 'Frutos del Bosque', 'Ingles', 'Negro', 'Albahaca', 'Menta Poleo', 'Tila'],
+        'Productos de Cafeteria': ['Leche', 'Leche de Avellanas', 'Cafe en Granos', 'Cafe en Granos Descafeinado', 'Galletas', 'Azucar Morena', 'Azucar Blanca', 'Sacarina', 'Colacao Sobres', 'Colacao Personal'],
+        'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
+        'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
+        'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    },
     'Restaurante Luna Beach' :{
         'Alcohol': {
             'Vodka': ['Smirnoff', 'Absolut', 'Grey Goose', 'Beldever', 'Nero'],
@@ -10,9 +66,9 @@ restaurantes = {
             'Aperitivos' : ['Campari', 'Ricard', 'Aperol', 'Martini Rojo', 'Martini Bianco', 'Vermut Petroni Rojo', 'Vermut Petroni Blanco', 'Fernet Branca']
         },
         'Vinos' : {
-        'Blanco' : ['Verdejo Pez Volador', 'Verdejo Selección Viña Cimbrón','Chardonnay Aresán Organic','Sauvignon Blanc Viña Cimbrón', 'Albariño Señorio de Rubiós', 'Godello Abad Dom Bueno', 'Chardonnay Chablis Domaine Camú', 'Baron de Ley 3 Viñas' ],
-        'Rosado' : ['Campo Nuevo Blush', 'Avaniel Tempranillo Rose', 'Uvam Pinot Grigio Blush', 'Fleurs de Prairie', 'Fleurs de Prairie 1,5L Magnum'],
-        'Tinto' : ['Ciudad Secreta Vino de Ronda', 'Alta Pavina Pinot Noir', 'Rioja Vega Semicrianza', 'Avaniel Tempranillo Red', 'Monteabellón Crianza', 'La Mateo', 'Hacienda Monasterio'],
+        'Blanco' : ['Pasion BIO Seco', 'Villanueva','Chan de Rosas','Chan de Rosas Magnum', 'Enate 2-3-4', 'Chablis', 'Entrechuelos', 'Mas Picosa Blanco', 'Pragustus', 'Abadia Marcier', 'Patito Feo', 'Max Ferd Richter Zepellin', 'Equilibrio', 'La Mateo Blanco Barrica', 'Menade Ecologico', 'Menade Magnum', 'Marques de Riscal Rueda Limousin', 'Puech-Haut', 'Love By Leoube', 'Libero Velo de Flor', 'Gerard Boulay', 'Finca La Olma', 'Bon Jan Blanc'],
+        'Rosado' : ['Pasion Bio', 'Love By Leoube', 'Protos Rosado', 'Love Rose Magnum', 'Clavellina', 'Puech-Haut', 'Fradei', 'Abadia Mercier', 'Lerin'],
+        'Tinto' : ['Viña Vieja', 'Entrechuelos', 'Love By Leoube', 'Habla del Silencio', 'Vallehondo', 'Puech-Haut', 'Mas Picosa Bio', 'Hacienda Monasterio', 'Protos 27', 'Un Joben Ribera', 'Hacienda Monasterio Magnum', 'Flor de Pingus', 'Vega Sicilia Valbuena 5º año', 'Pago de Quintana', 'Roda I Reserva', 'La Mateo Vendimia', 'La Mateo Vendimia Magnum', 'Finca Besaya Joven', 'Peña El Gato', 'Pago El Espino', 'Tadeo', 'Valeriano', 'Almirez','Bon Jan Negre', 'Pasion de Bobal' ],
         'Espumoso' : ['Pet Nat Natural', 'Pasion Gran Reserva', 'Laurent Perrier Blanc', 'Laurent Perrier Rose', 'Laurent Perrier Blanc de bkancs 750 ml', 'Pasion Cava BLanco', 'Pasion Cava Rose', 'Prosecco', 'Lume' ],
         },
         'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
@@ -24,5 +80,52 @@ restaurantes = {
         'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
         'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
         'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    }}
+
+restaurantes_cocina = {
+    'Restaurante Symbad': {
+        'Congelados' : ['Gambas Peladas','Gambas 18-24','Pulpo','Langostinos Tigre','Caravineros','Gambas Blancas', 'Gambas Rojas', 'Cocochas', 'Vieiras', 'Calamar', 'Lenguado', 'Salmon', 'Bacalao', 'Corvina', 'Rosada','Puntillitas','Boquerones', 'Almejas', 'Mejillones', 'Atun', 'Rodavallo'],
+        'Carnes' : ['Solomillo Cerdo', 'Solomillo Ternera', 'Entrecor Ternera', 'Chuletas de Cordero', 'Pechuga Pollo', 'Hamburguesa'],
+        'Especias': ['Sal Fina','Sal Gruesa','Sal Himalaya','Sal Maldon', 'Sal Ahumada','Comino','Jengibre Molido','Nueces Peladas', 'Chile Deshidratado', 'Polvo de Pescado','Polvo de Pollo','Polvo de Verduras', 'Polvo de Aromas', 'Pimenton Oro', 'Pimenton Dulce','Colorante Amarillo','Cebolla Molida', 'Ajo Molido','Pimienta Grano Negro','Pimienta Molida Blanca', 'Orégano', 'Cayena Picante', 'Sesamo Blanco','Sesamo Negro' ],
+        'Verduras y Similares': ['Tomate', 'Cebolla', 'Tomate Cherry', 'Cebolla Morada', 'Lechuga','Papas', 'Boniato', 'Pepino', 'Calabacin', 'Perejil', 'Cilantro'],
+        'Frutas' : ['Manzana', 'Melon', 'Pera', 'Sandia', 'Mora', 'Papaya', 'Frambuesa', 'Mango', 'Naranja', 'Mandarina', 'Limon', 'Lima', 'Arandanos', 'Melocoton', 'Piña'],
+        'Salsas': ['Tartara', ' Bearnesa', ' Berris', 'Mayonesa', 'Mayonesa Japonesa', 'Tabasco', 'Sweet Chilli', 'Mojo', 'Soja Sin Gluten', 'Mostaza', 'Ketchup'],
+        'Cereales y Derivados' : ['Arroz Numen', 'Arroz largo Basmati ', 'Pasta Penne', 'Pasta Tagliatelle', 'Spaguetti'],
+        'Reposteria' : ['Azucar Glass', 'Azucar Blanca','Azucar Morena','Nata', 'Miel Flores', 'Miel de caña', 'Cobertura Chocoloate Blanco', 'Cobertura Chocoloate Negro', 'Esencia Vainilla', 'Canela', 'Chocolate Polvo', 'Gelatina Sin Sabor'],
+        'Frutos Secos y Similares' : ['Nueces', 'Pasas', 'Piñones', 'Almendras', 'Almendras'],
+        'Lacteos y Charcuteria' : ['Queso Mozarella', 'Queso Feta', 'Queso Chedar Lonchas','Mantequilla', 'Jamon York', 'Tocino', 'Huevos'],
+        'Panes y Similares' : ['Hogaza', 'Centeno', 'Cereales','Regañá','Grisini'],
+        'Aceites y Similares' : ['Aceite Girasol 10L', 'Aceite Oliva 5L', 'Aceite Oliva 1L', 'Vinagre Balsamico'],
+        
+    },
+    'Restaurante Andy´S': {
+        'Congelados' : ['Gambas Peladas','Gambas 18-24','Pulpo','Langostinos Tigre','Caravineros','Gambas Blancas', 'Gambas Rojas', 'Cocochas', 'Vieiras', 'Calamar', 'Lenguado', 'Salmon', 'Bacalao', 'Corvina', 'Rosada','Puntillitas','Boquerones', 'Almejas', 'Mejillones', 'Atun', 'Rodavallo'],
+        'Carnes' : ['Solomillo Cerdo', 'Solomillo Ternera', 'Entrecor Ternera', 'Chuletas de Cordero', 'Pechuga Pollo', 'Hamburguesa'],
+        'Especias': ['Sal Fina','Sal Gruesa','Sal Himalaya','Sal Maldon', 'Sal Ahumada','Comino','Jengibre Molido','Nueces Peladas', 'Chile Deshidratado', 'Polvo de Pescado','Polvo de Pollo','Polvo de Verduras', 'Polvo de Aromas', 'Pimenton Oro', 'Pimenton Dulce','Colorante Amarillo','Cebolla Molida', 'Ajo Molido','Pimienta Grano Negro','Pimienta Molida Blanca', 'Orégano', 'Cayena Picante', 'Sesamo Blanco','Sesamo Negro' ],
+        'Verduras y Similares': ['Tomate', 'Cebolla', 'Tomate Cherry', 'Cebolla Morada', 'Lechuga','Papas', 'Boniato', 'Pepino', 'Calabacin', 'Perejil', 'Cilantro'],
+        'Frutas' : ['Manzana', 'Melon', 'Pera', 'Sandia', 'Mora', 'Papaya', 'Frambuesa', 'Mango', 'Naranja', 'Mandarina', 'Limon', 'Lima', 'Arandanos', 'Melocoton', 'Piña'],
+        'Salsas': ['Tartara', ' Bearnesa', ' Berris', 'Mayonesa', 'Mayonesa Japonesa', 'Tabasco', 'Sweet Chilli', 'Mojo', 'Soja Sin Gluten', 'Mostaza', 'Ketchup'],
+        'Cereales y Derivados' : ['Arroz Numen', 'Arroz largo Basmati ', 'Pasta Penne', 'Pasta Tagliatelle', 'Spaguetti'],
+        'Reposteria' : ['Azucar Glass', 'Azucar Blanca','Azucar Morena','Nata', 'Miel Flores', 'Miel de caña', 'Cobertura Chocoloate Blanco', 'Cobertura Chocoloate Negro', 'Esencia Vainilla', 'Canela', 'Chocolate Polvo', 'Gelatina Sin Sabor'],
+        'Frutos Secos y Similares' : ['Nueces', 'Pasas', 'Piñones', 'Almendras', 'Almendras'],
+        'Lacteos y Charcuteria' : ['Queso Mozarella', 'Queso Feta', 'Queso Chedar Lonchas','Mantequilla', 'Jamon York', 'Tocino', 'Huevos'],
+        'Panes y Similares' : ['Hogaza', 'Centeno', 'Cereales','Regañá','Grisini'],
+        'Aceites y Similares' : ['Aceite Girasol 10L', 'Aceite Oliva 5L', 'Aceite Oliva 1L', 'Vinagre Balsamico'],
+        
+    },
+    'Restaurante Luna Beach' :{
+        'Congelados' : ['Gambas Peladas','Gambas 18-24','Pulpo','Langostinos Tigre','Caravineros','Gambas Blancas', 'Gambas Rojas', 'Cocochas', 'Vieiras', 'Calamar', 'Lenguado', 'Salmon', 'Bacalao', 'Corvina', 'Rosada','Puntillitas','Boquerones', 'Almejas', 'Mejillones', 'Atun', 'Rodavallo'],
+        'Carnes' : ['Solomillo Cerdo', 'Solomillo Ternera', 'Entrecor Ternera', 'Chuletas de Cordero', 'Pechuga Pollo', 'Hamburguesa'],
+        'Especias': ['Sal Fina','Sal Gruesa','Sal Himalaya','Sal Maldon', 'Sal Ahumada','Comino','Jengibre Molido','Nueces Peladas', 'Chile Deshidratado', 'Polvo de Pescado','Polvo de Pollo','Polvo de Verduras', 'Polvo de Aromas', 'Pimenton Oro', 'Pimenton Dulce','Colorante Amarillo','Cebolla Molida', 'Ajo Molido','Pimienta Grano Negro','Pimienta Molida Blanca', 'Orégano', 'Cayena Picante', 'Sesamo Blanco','Sesamo Negro' ],
+        'Verduras y Similares': ['Tomate', 'Cebolla', 'Tomate Cherry', 'Cebolla Morada', 'Lechuga','Papas', 'Boniato', 'Pepino', 'Calabacin', 'Perejil', 'Cilantro'],
+        'Frutas' : ['Manzana', 'Melon', 'Pera', 'Sandia', 'Mora', 'Papaya', 'Frambuesa', 'Mango', 'Naranja', 'Mandarina', 'Limon', 'Lima', 'Arandanos', 'Melocoton', 'Piña'],
+        'Salsas': ['Tartara', ' Bearnesa', ' Berris', 'Mayonesa', 'Mayonesa Japonesa', 'Tabasco', 'Sweet Chilli', 'Mojo', 'Soja Sin Gluten', 'Mostaza', 'Ketchup'],
+        'Cereales y Derivados' : ['Arroz Numen', 'Arroz largo Basmati ', 'Pasta Penne', 'Pasta Tagliatelle', 'Spaguetti'],
+        'Reposteria' : ['Azucar Glass', 'Azucar Blanca','Azucar Morena','Nata', 'Miel Flores', 'Miel de caña', 'Cobertura Chocoloate Blanco', 'Cobertura Chocoloate Negro', 'Esencia Vainilla', 'Canela', 'Chocolate Polvo', 'Gelatina Sin Sabor'],
+        'Frutos Secos y Similares' : ['Nueces', 'Pasas', 'Piñones', 'Almendras', 'Almendras'],
+        'Lacteos y Charcuteria' : ['Queso Mozarella', 'Queso Feta', 'Queso Chedar Lonchas','Mantequilla', 'Jamon York', 'Tocino', 'Huevos'],
+        'Panes y Similares' : ['Hogaza', 'Centeno', 'Cereales','Regañá','Grisini'],
+        'Aceites y Similares' : ['Aceite Girasol 10L', 'Aceite Oliva 5L', 'Aceite Oliva 1L', 'Vinagre Balsamico'],
         
     }}
--- productos_limpieza.py
+++ productos_limpieza.py
@@ -1,4 +1,28 @@
 restaurantes = {
+    'Restaurante Symbad': {
+        'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
+        'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
+        'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
+        'Zumos': ['Manzana 20cl', 'Melocoton 20cl', 'Tomate 20cl', 'Arandanos', 'Piña 20cl','Manzana Carton 1L', 'Naranja Carton 1L', 'Melocoton Carton 1L', 'Tomate Carton 1L', 'Arandanos Carton 1L', 'Piña Carton 1L'],
+        'Infuciones' : ['Manzanilla', 'Verde', 'Rojo', 'Frutos del Bosque', 'Ingles', 'Negro', 'Albahaca', 'Menta Poleo', 'Tila'],
+        'Productos de Cafeteria': ['Leche', 'Leche de Avellanas', 'Cafe en Granos', 'Cafe en Granos Descafeinado', 'Galletas', 'Azucar Morena', 'Azucar Blanca', 'Sacarina', 'Colacao Sobres', 'Colacao Personal'],
+        'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
+        'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
+        'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    },
+    'Restaurante Andy´S': {
+        'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
+        'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
+        'Refrescos': ['Agua con Gas Solan de Cabras','Agua Cristal 33cl','Agua Plastico 1L','Lipton','CocaCola 33cl','CocaCola Zero 33cl', '7up 33cl', 'Fanta Limon 33cl', 'Fanta Naranja 33cl','Aquarius Limon 33cl','Aquarius Naranja 33cl', 'Ginger Ale', 'Tonica Sweps', 'Tonica Sweps Zero','Soda Sweps ','Casera 50cl', 'Tonica Premium Fruta Roja','Gingerale Sweps','Ginger Beer', 'Tonica Fever-Tree','Granadina', 'Lima Cordial' ],
+        'Zumos': ['Manzana 20cl', 'Melocoton 20cl', 'Tomate 20cl', 'Arandanos', 'Piña 20cl','Manzana Carton 1L', 'Naranja Carton 1L', 'Melocoton Carton 1L', 'Tomate Carton 1L', 'Arandanos Carton 1L', 'Piña Carton 1L'],
+        'Infuciones' : ['Manzanilla', 'Verde', 'Rojo', 'Frutos del Bosque', 'Ingles', 'Negro', 'Albahaca', 'Menta Poleo', 'Tila'],
+        'Productos de Cafeteria': ['Leche', 'Leche de Avellanas', 'Cafe en Granos', 'Cafe en Granos Descafeinado', 'Galletas', 'Azucar Morena', 'Azucar Blanca', 'Sacarina', 'Colacao Sobres', 'Colacao Personal'],
+        'Bag In Box' : ['Pepsi', 'Kas Naranja', 'Kas Limon', '7Up', 'Pepsi Light'],
+        'Productos Cocteleria' : ['Pure Piña Colada', 'Pure Fresa', ' Pure Pasion', 'Pure Frambuesa', 'Pure Mango', 'Pure Platano', 'Sirope Coco', 'Sirope Fresa', 'Sirope Vainilla', 'Sirope Violeta', 'Frape Yogurt', 'Frape Vainilla', 'Frape Café' ],
+        'Licores Cocteleria' : ['Ron Añejo Bucanero', 'Triple Seco', 'Cachaça', 'Tequila Olmeca Blanco', 'Vodka Stolichnaya', 'Gin Gordon´s', 'Whisky J&B'],
+        
+    },
     'Restaurante Luna Beach' :{
         'Cervezas' : ['Budweiser','Desperados','Sol','CruzCampo Botellin','CruzCampo Sin Gluten', 'El Aguila', 'Heineken Retornables', 'Heineken 0,0% Retornables', 'Mageners', 'Sol', 'Kopparberg MIX', 'Strongbow', 'Stella Artois'],
         'Barriles' : ['Barril CruzCampo', 'Barril Latino Limon', 'Barril Latino Casera', 'Barril Cerveza 0/0','Carbonico'],
