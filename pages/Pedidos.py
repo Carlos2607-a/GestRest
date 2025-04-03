1ba86abe98c29eec4169ef95395808773d00fdc1
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2025-04-03 19:28:51 +0000

    listo

--- pages/Pedidos.py
+++ pages/Pedidos.py
@@ -1,17 +1,8 @@
 import pandas as pd
-import base64
-from io import BytesIO
 from datetime import datetime
 import streamlit as st
-import altair as alt
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-import os
 from funciones import añadir_producto, to_excel, descargar_excel, seleccionar_productos, vista_previa, mostrar_carrito, seleccionar_productos_cocina
 from productos import restaurantes, restaurantes_cocina
-
 
 st.set_page_config(
     page_title='Solicitud de Productos',
@@ -20,19 +11,19 @@ st.set_page_config(
 
 st.title('Solicitud de Productos')
 
-
 # Crea una lista de subcategorías para 'Pedidos'
 subcategorias = ['Productos Cocina', 'Productos Barra', 'Productos Limpieza']
 # Crea un widget selectbox en la barra lateral para seleccionar la subcategoría
 subcategoria_seleccionada = st.selectbox('Selecciona una subcategoría', subcategorias)
+
+# Asegurémonos de que 'pedidos' esté siempre presente en la sesión, para evitar sobreescritura
+if 'pedidos' not in st.session_state:
+    st.session_state['pedidos'] = pd.DataFrame(columns=['Producto'])
+
 # Muestra el contenido correspondiente a la subcategoría seleccionada
 if subcategoria_seleccionada == 'Productos Cocina':
-
     st.write('Productos Cocina')
     
-    if 'pedidos' not in st.session_state:
-        st.session_state['pedidos'] = pd.DataFrame(columns=['Producto'])
-
     # Selección de restaurante
     restaurante_seleccionado = st.selectbox('Selecciona el Restaurante', [''] + list(restaurantes_cocina.keys()))
 
@@ -48,19 +39,14 @@ if subcategoria_seleccionada == 'Productos Cocina':
         # Vista previa del DataFrame
         vista_previa(st.session_state['pedidos'])
 
-        if 'pedidos' in st.session_state:
-            mostrar_carrito(st.session_state['pedidos'])
+        # Mostrar el carrito
+        mostrar_carrito(st.session_state['pedidos'])
 
     nombre_archivo = f"Pedido_{restaurante_seleccionado}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
     descargar_excel(st.session_state['pedidos'], nombre_archivo)
 
-
 elif subcategoria_seleccionada == 'Productos Barra':
-   
     st.write('Productos de Barra')
-
-    if 'pedidos' not in st.session_state:
-        st.session_state['pedidos'] = pd.DataFrame(columns=['Producto'])
 
     # Selección de restaurante
     restaurante_seleccionado = st.selectbox('Selecciona el Restaurante', [''] + list(restaurantes.keys()))
@@ -77,8 +63,8 @@ elif subcategoria_seleccionada == 'Productos Barra':
     # Vista previa del DataFrame
     vista_previa(st.session_state['pedidos'])
 
-    if 'pedidos' in st.session_state:
-        mostrar_carrito(st.session_state['pedidos'])
+    # Mostrar el carrito
+    mostrar_carrito(st.session_state['pedidos'])
 
     nombre_archivo = f"Pedido_{restaurante_seleccionado}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
     descargar_excel(st.session_state['pedidos'], nombre_archivo)
@@ -86,9 +72,6 @@ elif subcategoria_seleccionada == 'Productos Barra':
 elif subcategoria_seleccionada == 'Productos Limpieza':
     st.write('Productos Limpieza')
 
-    if 'pedidos' not in st.session_state:
-        st.session_state['pedidos'] = pd.DataFrame(columns=['Producto'])
-
     # Selección de restaurante
     restaurante_seleccionado = st.selectbox('Selecciona el Restaurante', [''] + list(restaurantes.keys()))
 
@@ -104,12 +87,12 @@ elif subcategoria_seleccionada == 'Productos Limpieza':
     # Vista previa del DataFrame
     vista_previa(st.session_state['pedidos'])
 
-    if 'pedidos' in st.session_state:
-        mostrar_carrito(st.session_state['pedidos'])
+    # Mostrar el carrito
+    mostrar_carrito(st.session_state['pedidos'])
 
     nombre_archivo = f"Pedido_{restaurante_seleccionado}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
     descargar_excel(st.session_state['pedidos'], nombre_archivo)
-    
 
+# Si el botón "Volver al Menú Principal" es presionado, recargar la página principal
 if st.button("Volver al Menú Principal"):
-    st.page_link("Home.py", disabled=False)
+    st.experimental_rerun()  # Esto recarga la aplicación
